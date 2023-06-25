59aee3159dff3b3d943008f22623924812f9ed2f
kaykegy@proton.me <kaykegy@proton.me>
2023-06-25 01:18:19 -0300

    PrimeFactors

--- Prime Factors_6kyu/PrimeFactors.c
+++ Prime Factors_6kyu/PrimeFactors.c
@@ -1,65 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PrimeFactors.c                                     :+:      :+:    :+:   */
+/*   Prime_factors.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kayke <kayke@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 14:34:58 by kayke             #+#    #+#             */
-/*   Updated: 2023/06/24 19:07:50 by kayke            ###   ########.fr       */
+/*   Updated: 2023/06/25 01:11:45 by kayke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int is_prime(unsigned long long num){
-  unsigned long long count = 2;
-
-  if (num < 2)
-    return 0;
-
-  while(count < num){
-    if (num % count == 0)
-      return 0;
-
-    count++;
-  }
-  return 1;
-}
-
 int prime_factors(unsigned long long n, unsigned long long factors[]) {
-    unsigned long long count = 2;
-    size_t factorIndex = 0; 
-    
-    while (count <= n) {
-        if (n % count == 0 && is_prime(count)) {
-            n = n / count;
-            factors[factorIndex] = count; 
-            printf("count: %llu\n", count);
-            count = 2;
-            factorIndex++; 
-            continue;
-        }
-        count++;
+    int index = 0;
+    int unsigned long long i = 3;
+    while (n % 2 == 0) 
+    {
+        factors[index++] = 2;
+        n = n / 2;
     }
-    factors[factorIndex] = 0;
-    return 0;
+    while (i * i <= n) 
+    {
+        while (n % i == 0) {
+            factors[index++] = i;
+            n = n / i;
+        }
+      i = i + 2;
+    }
+    if (n > 2) {
+        factors[index++] = n;
+    }
+    return index; 
 }
 
 int main() {
-    unsigned long long factors[10]; 
-    prime_factors(220, factors);
-    
-    unsigned long long i = 0;
-    printf("Fatores: ");
-    while (i < 5) {
-        if (factors[i] != 0) {
-            printf("%llu ", factors[i]);
-        }
-        i++;
+    unsigned long long n = 12;
+    unsigned long long factors[100];
+    int i = 0;
+    int numFactors = prime_factors(n, factors);
+
+    printf("Prime factors of %llu: ", n);
+    while (i < numFactors)
+    {
+        printf("%llu ", factors[i]);
+    i++;
     }
     printf("\n");
-
     return 0;
 }
