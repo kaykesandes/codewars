ef4984dcd6ee66a555ea0227f3eeef2ad0e4d18f
kaykesandes <kaykegy@proton.me>
2024-05-01 16:04:09 -0300

    main

---
+++ cpp/doc/a4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+//O type string ainda não é padrão cpp aqui está sendo usado outra biblioteca
+//Para a gente manipular string
+
+int main(){
+
+    std::string greeting = "Ola mundo";
+
+    std::cout << greeting;
+
+    return 0;
+}
